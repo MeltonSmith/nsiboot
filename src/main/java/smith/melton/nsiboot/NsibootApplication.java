0845bfd8d2eeee6bbcb35831403cc5dd7344cb27
Melton Smith <ravenglc1@gmail.com>
2020-10-20 22:44:29 +0500

    initial commit

---
+++ src/main/java/smith/melton/nsiboot/NsibootApplication.java
@@ -0,0 +1,13 @@
+package smith.melton.nsiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsibootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NsibootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/smith/melton/nsiboot/NsibootApplicationTests.java
@@ -0,0 +1,13 @@
+package smith.melton.nsiboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsibootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
