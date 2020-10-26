248ac9739149e8273d36756ab72b6db945639b29
MeltonSmith <ravenglc1@gmail.com>
2020-10-26 19:51:16 +0500

    minor refactoring

--- src/main/resources/apicurioAvro/utils/watchATopic.sh
+++ src/main/resources/apicurioAvro/utils/watchATopic.sh
@@ -1,3 +1,3 @@
-docker run -it --rm --name watcher --net apicuriojson_default -e ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_BROKER=kafka:9092 debezium/kafka:1.3 watch-topic -a -k \
+docker run -it --rm --name watcher --net apicurioavro_default -e ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_BROKER=kafka:9092 debezium/kafka:1.3 watch-topic -a -k \
 storage-topic
 $SHELL
